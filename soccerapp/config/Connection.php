70244ed5b0d0360c8b80a8c2fd843385b9fb33b6
cristiantirano27 <catirano5@misena.edu.co>
2023-10-06 15:41:21 -0500

    CHORE: [6/10/2022 - 03:40 pm]  Archivo de conexion a la BD del proyecto SOCCERAPP

---
+++ soccerapp/config/Connection.php
@@ -0,0 +1,26 @@
+<?php
+
+class Connection 
+{
+    private $driver = 'pgsql';
+    private $host = 'localhost';
+    private $port = '5432';
+    private $dbname = 'soccerapp';
+    private $user = 'postgres';
+    private $password = '54321';
+    private $connect;
+
+    static public function getConnection() {
+        try {
+            $connection = new Connection();
+            $connection->connect = new PDO("{$connection->driver}:host={$connection->host};port={$connection->port};dbname={$connection->dbname}",$connection->user,$connection->password);
+            $connection->connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Conectado a la BD";
+            return $connection->connect;
+        } catch (PDOException $e) {
+            echo "Error: ".$e->getMessage();
+        }
+    }
+
+}
+Connection::getConnection();
