e7f3fb1407697b4126bbb566e91c3836b8abd648
cristiantirano27 <catirano5@misena.edu.co>
2023-10-09 14:19:03 -0500

    CHORE: [9/10/2023 - 02:22 pm] Modificando el archivo de conexion a la BD del proyecto SOCERAPP :

--- soccerapp/config/Connection.php
+++ soccerapp/config/Connection.php
@@ -15,7 +15,7 @@ class Connection
             $connection = new Connection();
             $connection->connect = new PDO("{$connection->driver}:host={$connection->host};port={$connection->port};dbname={$connection->dbname}",$connection->user,$connection->password);
             $connection->connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "Conectado a la BD";
+            // echo "Conectado a la BD";
             return $connection->connect;
         } catch (PDOException $e) {
             echo "Error: ".$e->getMessage();
@@ -23,4 +23,4 @@ class Connection
     }
 
 }
-Connection::getConnection();
+// Connection::getConnection();
