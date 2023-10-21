5b56fae6107c4ce4fd7392efd105b9f86c8dfa37
cristiantirano27 <catirano5@misena.edu.co>
2023-10-20 20:17:13 -0500

    20/10/2023 Update Application:

--- App/core/Router.php
+++ App/core/Router.php
@@ -4,12 +4,39 @@ namespace app\core;
 
 class Router
 {
+    protected array $routes;
+
     public function __construct() 
     {
     }
 
-    public function setRouter(string $url, string $callback) 
+    public function setRoute(string $url, string $callback) 
     {
-        echo "Router : {$url} is set to call function: {$callback} </br>";
+        $this->routes[] = [$url, $callback];
+        // echo "Router : {$url} is set to call function: {$callback} </br>";
+    }
+
+    public function getRoutes()
+    {
+        return $this->routes;
+    }
+
+    public function getPath()
+    {
+        $uri = $_SERVER["REQUEST_URI"] ?? "/";    
+        $search = "/App";
+        $path = str_replace($search, "", $uri);
+
+        $search = "?";
+        $indexLocation = strpos($path, $search);
+
+        if (is_int($indexLocation)) 
+        {
+            $path = substr($path, 0, $indexLocation);
+        }
+
+        echo "<pre>";
+        var_dump($uri, $path);
+        echo "</pre>";
     }
 }
