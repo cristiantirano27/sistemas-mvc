c89928d9830974a77a11981cfc9164c7b5529246
cristiantirano27 <catirano5@misena.edu.co>
2023-11-01 16:22:46 -0500

    01/11/2023 | Constantes para la conexion a la BD, proyecto SPM

---
+++ spm/config/SERVER.php
@@ -0,0 +1,14 @@
+<?php
+
+# Constantes de conexión a la bd
+const SERVER = "localhost";
+const DB = "spm";
+const USER = "root";
+const PASS = "";
+
+const SGBD = "mysql:host=".SERVER.";dbname=".DB;
+
+# Constantes de encriptación
+const METHOD = "AES-256-CBC";
+const SECRET_KEY = '$spm@2023';
+const SECRET_IV = '271000';
