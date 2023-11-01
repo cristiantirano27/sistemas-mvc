cbb9d8d67f90c06b324e2ad6b7411465a93a7475
cristiantirano27 <catirano5@misena.edu.co>
2023-11-01 16:21:14 -0500

    01/11/2023 | Constantes globales para el sistema, proyecto SPM

---
+++ spm/config/APP.php
@@ -0,0 +1,10 @@
+<?php
+
+# Constantes globales del sistema
+const SERVERURL = "http://localhost/spm/";
+
+const COMPANY = "SISTEMA DE PRESTAMOS MOBILIARIOS";
+
+const MODEDA = "$";
+
+date_default_timezone_set("America/Bogota");
