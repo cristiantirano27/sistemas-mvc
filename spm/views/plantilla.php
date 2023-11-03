f4ace8b772f19ab32a2d049e9c912218bcf19c8a
cristiantirano27 <catirano5@misena.edu.co>
2023-11-03 11:31:47 -0500

    03/11/2023 | Actualización de la plantilla, proyecto SPM

--- spm/views/plantilla.php
+++ spm/views/plantilla.php
@@ -1,3 +1,25 @@
-<?php
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title><?php echo COMPANY; ?></title>
+    <?php include "./views/inc/Links.php"; ?>
 
-echo "Hola Mundo";
+</head>
+<body>
+	
+	<!-- Main container -->
+	<main class="full-box main-container">
+		<!-- Nav lateral -->
+		<?php include "./views/inc/NavLateral.php"; ?>
+
+		<!-- Page content -->
+		<section class="full-box page-content">
+			<?php include "./views/inc/NavBar.php"; ?>
+		</section>
+	</main>
+	
+	<?php include "./views/inc/Scripts.php"; ?>
+</body>
+</html>
