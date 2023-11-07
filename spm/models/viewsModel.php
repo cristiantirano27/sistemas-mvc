25ebfc41cbe5e7ef4d21fe6951cf853c2ed4dbd4
cristiantirano27 <catirano5@misena.edu.co>
2023-11-07 16:55:03 -0500

    07/11/2023 | Actualización del modelo para las vistas del sistema, proyecto SPM

---
+++ spm/models/viewsModel.php
@@ -0,0 +1,21 @@
+<?php
+
+class viewsModel {
+    # Modelo para obtener las vistas del sistema
+    protected static function obtener_vista_modelo($vistas)
+    {
+        $listaBlanca = ["home", "client-list"];    
+        if (in_array($vistas, $listaBlanca)) {
+            if (is_file("./views/contents/".$vistas."-view.php")) {
+                $contenido = "./views/contents/".$vistas."-view.php"; 
+            } else {
+                $contenido = "404";
+            }
+        } elseif ($vistas == "login" || $vistas == "index") {
+            $contenido = "login";
+        } else {
+            $contenido = "404";
+        }
+        return $contenido;
+    }
+}
