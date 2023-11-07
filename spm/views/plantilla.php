af1adb22c84584d8e3034c05d2482fb5f7ffc383
cristiantirano27 <catirano5@misena.edu.co>
2023-11-07 16:52:06 -0500

    07/11/2023 | Actualización plantilla, proyecto SPM

--- spm/views/plantilla.php
+++ spm/views/plantilla.php
@@ -8,18 +8,34 @@
 
 </head>
 <body>
-	
-	<!-- Main container -->
-	<main class="full-box main-container">
-		<!-- Nav lateral -->
-		<?php include "./views/inc/NavLateral.php"; ?>
+	<?php
+		$peticionesAjax = false;
+		require_once "./controllers/viewsController.php";
+		$IV = new viewsController();
 
-		<!-- Page content -->
-		<section class="full-box page-content">
-			<?php include "./views/inc/NavBar.php"; ?>
-		</section>
-	</main>
-	
-	<?php include "./views/inc/Scripts.php"; ?>
+		$vistas = $IV->obtener_vistas_coontrolador();
+
+		if ($vistas == "login" || $vistas == "404") {
+			require_once "./views/contents/".$vistas."-view.php";
+		} else {
+		?>
+			<!-- Main container -->
+			<main class="full-box main-container">
+			<!-- Nav lateral -->
+			<?php include "./views/inc/NavLateral.php"; ?>
+
+			<!-- Page content -->
+			<section class="full-box page-content">
+				<?php 
+					include "./views/inc/NavBar.php"; 
+
+					include $vistas;
+				?>
+			</section>
+		</main>
+		<?php 
+			}
+			include "./views/inc/Scripts.php"; 
+		?>
 </body>
 </html>
