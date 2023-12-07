7083a89ae79ce6a5e0e11db6ce22b0a51a5da075
cristiantirano27 <catirano5@misena.edu.co>
2023-12-07 13:53:09 -0500

    07/12/2023 | Actualización de archivo para guardar peticiones ajax (usuario), proyecto SPM

--- spm/ajax/userAjax.php
+++ spm/ajax/userAjax.php
@@ -3,7 +3,7 @@
     $peticionAjax = true;
     require_once "../config/APP.php";
 
-    if (isset($_POST['usuario_no_id_reg'])) {
+    if (isset($_POST['usuario_no_id_reg']) || isset($_POST['usuario_id_del'])) {
         /* Instancia al controlador */
         require_once "../controllers/userController.php";
         $ins_usuario = new userController();
@@ -11,6 +11,11 @@
         /* Agregando un usuario */
         if (isset($_POST['usuario_no_id_reg']) && isset($_POST['usuario_nombre_reg']) && isset($_POST['usuario_apellido_reg']) && isset($_POST['usuario_telefono_reg']) && isset($_POST['usuario_direccion_reg'])) {
             echo $ins_usuario->agregar_usuario_controlador();
+        }
+
+        /* Agregando un usuario */
+        if (isset($_POST['usuario_id_del'])) {
+            echo $ins_usuario->eliminar_usuario_controlador();
         }
     } else {
         session_start(['name' => 'LS']);
