867e62a4ee87ececeb8e66306ac5159b7db9e1a8
cristiantirano27 <catirano5@misena.edu.co>
2024-06-21 11:10:49 -0500

    21/06/2024 | Actualizando la plantilla base del sistema

--- SPM/vistas/plantilla.php
+++ SPM/vistas/plantilla.php
@@ -1,3 +1,22 @@
-<?php
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title><?php echo COMPANY; ?></title>
+    <?php require_once "./vistas/inc/Link.php"; ?>
+</head>
+<body>
+	<!-- Main container -->
+	<main class="full-box main-container">
+		<!-- Nav lateral -->
+		<?php require_once "./vistas/inc/NavLateral.php"; ?>
 
-    echo "<h1>Hola Mundo</h1>";
+		<!-- Page content -->
+		<section class="full-box page-content">
+	        <?php require_once "./vistas/inc/NavBar.php"; ?>
+    	</section>
+	</main>
+	<?php require_once "./vistas/inc/Scripts.php"; ?>	
+</body>
+</html>
